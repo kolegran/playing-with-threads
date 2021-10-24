8a85ef23d0b5245b63ffa1f7164d2e97b08617c4
kolegran <kolegran@gmail.com>
2021-10-24 20:16:57 +0300

    Fixed problem with overwriting of content because of parallel access and append to the end of file instead of overwriting

--- src/main/java/com/github/kolegran/ConcurrentResourcesAccess.java
+++ src/main/java/com/github/kolegran/ConcurrentResourcesAccess.java
@@ -14,12 +14,15 @@ public class ConcurrentResourcesAccess {
         Files.deleteIfExists(Paths.get(FILE));
 
         final File file = new File(FILE);
+        file.createNewFile();
 
         final String helloString = "Hello world!\n";
         final Thread hello = new Thread(() -> writeToFile(helloString, file), "hello");
 
         final String nameString = "My name is Kolegran.\n";
-        final Thread name = new Thread(() -> writeToFile(nameString, file), "name");
+        final Thread name = new Thread(() -> {
+            writeToFile(nameString, file);
+        }, "name");
 
         name.start();
         hello.start();
@@ -30,13 +33,23 @@ public class ConcurrentResourcesAccess {
         System.out.println(Files.readString(Paths.get(FILE)));
     }
 
-    // Currently both threads write to file in parallel, which leads to some characters being overwritten
-    private static void writeToFile(String content, File file) {
-        char[] chars = content.toCharArray();
+    private static synchronized void writeToFile(String content, File file) {
+        System.out.println(Thread.currentThread().getName());
+
+        char[] chars = readFileAndAppendNewContent(content).toCharArray();
         try (final FileOutputStream out = new FileOutputStream(file)) {
             for (char c : chars) {
                 out.write(c);
             }
+            System.out.println("TEST - " + Files.readString(Paths.get(FILE)));
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    private static String readFileAndAppendNewContent(String content) {
+        try {
+            return Files.readString(Paths.get(FILE)) + content;
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
