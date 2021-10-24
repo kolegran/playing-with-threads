67af19e864686c22cceaba09e2660cee066f0cb2
kolegran <kolegran@gmail.com>
2021-10-24 19:56:01 +0300

    Problem with parallel write access to file

--- src/main/java/com/github/kolegran/ConcurrentResourcesAccess.java
+++ src/main/java/com/github/kolegran/ConcurrentResourcesAccess.java
@@ -1,9 +1,10 @@
 package com.github.kolegran;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 
 public class ConcurrentResourcesAccess {
 
@@ -12,18 +13,13 @@ public class ConcurrentResourcesAccess {
     public static void main(String[] args) throws IOException, InterruptedException {
         Files.deleteIfExists(Paths.get(FILE));
 
+        final File file = new File(FILE);
+
         final String helloString = "Hello world!\n";
-        final Thread hello = new Thread(() -> writeToFile(helloString), "hello");
+        final Thread hello = new Thread(() -> writeToFile(helloString, file), "hello");
 
         final String nameString = "My name is Kolegran.\n";
-        final Thread name = new Thread(() -> {
-            try {
-                hello.join();
-            } catch (InterruptedException exception) {
-                exception.printStackTrace();
-            }
-            writeToFile(nameString);
-        }, "name");
+        final Thread name = new Thread(() -> writeToFile(nameString, file), "name");
 
         name.start();
         hello.start();
@@ -34,9 +30,12 @@ public class ConcurrentResourcesAccess {
         System.out.println(Files.readString(Paths.get(FILE)));
     }
 
-    private static void writeToFile(String content) {
-        try {
-            Files.writeString(Paths.get(FILE), content, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+    private static void writeToFile(String content, File file) {
+        char[] chars = content.toCharArray();
+        try (final FileOutputStream out = new FileOutputStream(file)) {
+            for (char c : chars) {
+                out.write(c);
+            }
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
